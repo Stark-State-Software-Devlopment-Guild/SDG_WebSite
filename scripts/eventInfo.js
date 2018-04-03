cf615dd26c212d97f631a5ac8593784f8cfe5ca8
QualebTheTurtle <caleb.wagner365@gmail.com>
2018-04-03 01:28:23 -0400

    updated event info to only show on the proper month and year

--- scripts/eventInfo.js
+++ scripts/eventInfo.js
@@ -29,34 +29,48 @@ function eventData(){
 *creates a HTML table of 3 rows containing a name header, a smaller date header, and a description
 */
 function printEventData(name, month, day, year, description){
-	//create <table></table>
-	var table = document.createElement('table');
-	//create <tr></tr>
-	var tr = document.createElement('tr');
-	//create <td></td>
-	var td = document.createElement('td');
-	//.innerHTML sets the HTML within the tags
-	td.innerHTML = "<h3>"+name+"</h3>";
-	//defines that td is the last item within tr
-	tr.appendChild(td);
-	//defines that tr is the last child element of table
-	table.appendChild(tr); 
+	//get month and year form calendar
+	var currentMonthYearDiv = document.getElementById("calendar-month-year");
 
-	//similar to above
-	var tr = document.createElement('tr');
-	var td = document.createElement('td');
-	td.innerHTML = "<h6>"+month+" "+day+" "+year+"</h6>";
-	tr.appendChild(td);
-	table.appendChild(tr);
-	var tr = document.createElement('tr');
-	var td = document.createElement('td');
-	td.innerHTML = description;
-	tr.appendChild(td);
-	table.appendChild(tr);
+	//split by space
+	var monthYearArr = currentMonthYearDiv.innerHTML.split(" ");
 
-	/*
-	*searches the HTML for the first tag with the id = "container-events"
-	*then sets the last child element of the element to the table HTML object
-	*/	
-	document.getElementById("container-events").appendChild(table);
+	//month and year variables
+	var selectedMonth = monthYearArr[0];
+	var selectedYear = monthYearArr[1];
+
+	//only show data for selected month and year
+	if (month == selectedMonth && year == selectedYear){
+		//create <table></table>
+		var table = document.createElement('table');
+		//create <tr></tr>
+		var tr = document.createElement('tr');
+		//create <td></td>
+		var td = document.createElement('td');
+		//.innerHTML sets the HTML within the tags
+		td.innerHTML = "<h3>"+name+"</h3>";
+		//defines that td is the last item within tr
+		tr.appendChild(td);
+		//defines that tr is the last child element of table
+		table.appendChild(tr); 
+
+		//similar to above
+		var tr = document.createElement('tr');
+		var td = document.createElement('td');
+		td.innerHTML = "<h6>"+month+" "+day+" "+year+"</h6>";
+		tr.appendChild(td);
+		table.appendChild(tr);
+		//similar to above
+		var tr = document.createElement('tr');
+		var td = document.createElement('td');
+		td.innerHTML = description;
+		tr.appendChild(td);
+		table.appendChild(tr);
+
+		/*
+		*searches the HTML for the first tag with the id = "container-events"
+		*then sets the last child element of the element to the table HTML object
+		*/	
+		document.getElementById("container-events").appendChild(table);
+	}
 }
